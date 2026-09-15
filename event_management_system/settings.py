9c0ff4e3a003968a369b604771b4add988506be1
AdityaManojShinde <aditya.manoj.shinde@gmail.com>
2026-09-15 12:02:10 +0530

    style fix: added static root to settings

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # storage setup
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
